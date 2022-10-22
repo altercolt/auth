64cf42306ede68cbf77e5cd7762697c4246cbf99
altercolt <aybarrel@gmail.com>
2022-10-23 00:03:35 +0600

    added repository interfaces and their implementations

--- internal/core/auth/repository.go
+++ internal/core/auth/repository.go
@@ -1,8 +1,15 @@
 package auth
 
-import "github.com/google/uuid"
+import (
+	"context"
+	"github.com/google/uuid"
+)
 
 type TokenRepository interface {
+	Create(ctx context.Context, token RefreshToken) error
+	Delete(ctx context.Context, id uuid.UUID) error
+	Fetch(ctx context.Context, filter Filter) ([]RefreshToken, error)
+	FetchOne(ctx context.Context, filter Filter) (RefreshToken, error)
 }
 
 type Filter struct {
--- internal/core/user/repository.go
+++ internal/core/user/repository.go
@@ -1,6 +1,15 @@
 package user
 
+import (
+	"context"
+)
+
 type Repository interface {
+	Create(ctx context.Context, m Model) error
+	Delete(ctx context.Context, id int) error
+	Update(ctx context.Context, m Model) error
+	Fetch(ctx context.Context, filter Filter) ([]User, error)
+	FetchOne(ctx context.Context, filter Filter) (User, error)
 }
 
 type Filter struct {
